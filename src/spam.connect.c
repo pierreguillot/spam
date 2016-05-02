33671591f0f1b654939b9263b5936687b5cf0750
pierreguillot <guillotpierre6@gmail.com>
2016-05-02 17:56:26 +0200

    spam.connect (because 64 channels...)

--- src/spam.connect.c
+++ src/spam.connect.c
@@ -29,102 +29,41 @@ static void *spam_connect_new()
 
 static void spam_connect_bang(t_spam_connect *x)
 {
-    int i, j, index = 0;
-    int x1 = 0, x2 = 0, y1 = 0, y2 = 0;
-    int nx1, nx2, ny1, ny2;
-    t_gobj *y = NULL;
-    t_gobj *list[512];
+    int nouts, nins, i;
+    t_object *first = NULL, *second = NULL;
+    int visible  = (x->s_canvas) ? glist_isvisible(x->s_canvas) : 0;
+    t_editor* ed = (x->s_canvas) ? x->s_canvas->gl_editor : NULL;
+    t_selection *sel;
     t_outconnect *oc;
-    t_symbol* name;
-    t_canvas* mcnv = x->s_canvas;
-    t_canvas* cnv;
-    
-    if(mcnv)
+    if(ed && visible)
     {
-        for(i = 0; i < 512; i++)
+        for(sel = ed->e_selection; sel; sel = sel->sel_next)
         {
-            list[i] = NULL;
-        }
-        for(y = mcnv->gl_list; y; y = y->g_next)
-        {
-            if(strncmp((const char *)class_getname(y->g_pd), "spam.", 5) == 0 && glist_isselected(mcnv, y))
+            if(!strncmp((const char *)class_getname(sel->sel_what->g_pd), "spam", 4))
             {
-                gobj_getrect(y, mcnv, &nx1, &ny1, &nx2, &ny2);
-                for(i = 0; i < index; i++)
+                if(first)
                 {
-                    gobj_getrect(list[i], mcnv, &x1, &y1, &x2, &y2);
-                    if(ny1 < y1 || (ny1 == y1 && nx1 < x1))
+                    second = (t_object *)sel->sel_what;
+                    nouts= obj_noutlets(second);
+                    nins = obj_ninlets(first);
+                    for(i = 0; i < nouts && i < nins; ++i)
                     {
-                        for(j = index; j > i; j--)
-                        {
-                            list[j] = list[j-1];
-                        }
-                        list[i] = y;
-                        index = (index + 1 > 255) ? 255 : index+1;
-                        break;
+                        obj_disconnect(second, i, first, i);
+                        oc = obj_connect(second, i, first, i);
+                        sys_vgui((char *)".x%lx.c create line %d %d %d %d -width %d -fill black -tags [list l%lx cord]\n",
+                                 x->s_canvas, 0, 0, 0, 0, (obj_issignaloutlet(second, i) ? 2 : 1), oc);
                     }
+                    canvas_fixlinesfor(x->s_canvas, (t_text *)second);
+                    first  = second;
+                    second = NULL;
                 }
-                if(i == index)
+                else
                 {
-                    list[index] = y;
-                    index = (index + 1 > 255) ? 255 : index+1;
-                }
-            }
-            else if(y->g_pd == canvas_class)
-            {
-                cnv = (t_glist *)y;
-                if(cnv->gl_obj.te_binbuf && binbuf_getnatom(cnv->gl_obj.te_binbuf) && binbuf_getvec(cnv->gl_obj.te_binbuf))
-                {
-                    if(binbuf_getvec(cnv->gl_obj.te_binbuf)->a_type == A_SYMBOL)
-                        name = atom_getsymbol(binbuf_getvec(cnv->gl_obj.te_binbuf));
-                    else
-                        name  = NULL;
-                    
-                    if(name && strncmp(name->s_name, "spam.", 5) == 0 && glist_isselected(mcnv, y))
-                    {
-                        gobj_getrect(y, mcnv, &nx1, &ny1, &nx2, &ny2);
-                        for(i = 0; i < index; i++)
-                        {
-                            gobj_getrect(list[i], mcnv, &x1, &y1, &x2, &y2);
-                            if(ny1 < y1 || (ny1 == y1 && nx1 < x1))
-                            {
-                                for(j = index; j > i; j--)
-                                {
-                                    list[j] = list[j-1];
-                                }
-                                list[i] = y;
-                                index = (index + 1 > 255) ? 255 : index+1;
-                                break;
-                            }
-                        }
-                        if(i == index)
-                        {
-                            list[index] = y;
-                            index = (index + 1 > 255) ? 255 : index+1;
-                        }
-                    }
-                    
+                    first = (t_object *)sel->sel_what;
                 }
             }
         }
-        for(i = 0; i < index-1; i++)
-        {
-            for(j = 0; j < obj_noutlets((t_object *)list[i]) && j < obj_ninlets((t_object *)list[i+1]); j++)
-            {
-                obj_disconnect((t_object *)list[i], j, (t_object *)list[i+1], j);
-                oc = obj_connect((t_object *)list[i], j, (t_object *)list[i+1], j);
-                if(glist_isvisible(mcnv))
-                {
-                    sys_vgui((char *)".x%lx.c create line %d %d %d %d -width %d -fill black -tags [list l%lx cord]\n",
-                             glist_getcanvas(mcnv), 0, 0, 0, 0,
-                             (obj_issignaloutlet((t_object *)list[i], j) ? 2 : 1),
-                             oc);
-                }
-                
-            }
-            canvas_fixlinesfor(mcnv, (t_text *)list[i]);
-        }
-        canvas_dirty(mcnv, 1);
+        canvas_dirty(x->s_canvas, 1);
     }
 }
 
